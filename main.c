bf4c63c5d1c5c2e7e0de599a52c131abcdf047d5
Mariana Peres <marianaperes@myyahoo.com>
2025-10-21 22:07:18 -0300

    Atualizar função main para calcular e exibir o resultado da divisão de dois números inteiros; adicionar arquivo executável.

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int main () {
-    int a = 5, b = 3;
-    double media = (a + b) / 2.0;
-
-    printf("A média é: %.2f\n", media);
+    int a = 4, b = 2;
+    int resultado = a/b;
+    printf("%d\n", resultado);
     return 0;
 }
 
